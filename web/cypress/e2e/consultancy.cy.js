d8e4568ac563c729516620ade953eb21776ab0da
Diogo Brumassio <dibrunspeed@gmail.com>
2026-05-26 00:10:42 -0300

    Timeout alterado, testes para formulários incompletos

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -1,5 +1,5 @@
 describe("Formulário Consultoria", () => {
-  it("Deve solicitar consultoria individual", () => {
+  it.only("Deve solicitar consultoria individual", () => {
     cy.start();
     cy.submitLoginForm("papito@webdojo.com", "katana123");
     cy.goTo("Formulários", "Consultoria");
@@ -93,8 +93,41 @@ describe("Formulário Consultoria", () => {
 
     cy.contains("button", "Enviar formulário").click();
 
-    cy.contains(
-      "Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.",
-    ).should("be.visible");
+    cy.get(".modal")
+      .find(".modal-content")
+      .should("be.visible")
+      .and(
+        "have.text",
+        "Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.",
+      );
+  });
+
+  //ENVIANDO O FORMULÁRIO VAZIO PARA VALIDAR AS MENSAGENS DE ERRO
+  it("Deve solicitar consultoria in company", () => {
+    cy.start();
+    cy.submitLoginForm("papito@webdojo.com", "katana123");
+    cy.goTo("Formulários", "Consultoria");
+    cy.contains("button", "Enviar formulário").click();
+
+    cy.contains("label", "Nome Completo")
+      .parent()
+      .find("p")
+      .should("be.visible")
+      .and("have.class", "text-red-400")
+      .and("have.css", "color", "rgb(248, 113, 113)");
+
+    cy.contains("label", "Email")
+      .parent()
+      .find("p")
+      .should("be.visible")
+      .and("have.class", "text-red-400")
+      .and("have.css", "color", "rgb(248, 113, 113)");
+
+    cy.contains("label", "termos de uso")
+      .parent()
+      .find("p")
+      .should("be.visible")
+      .and("have.class", "text-red-400")
+      .and("have.css", "color", "rgb(248, 113, 113)");
   });
 });
